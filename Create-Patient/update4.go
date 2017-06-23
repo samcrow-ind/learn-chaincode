79fb20fd621881bb3f787c0f1dec51951f2c1ecd
samcrow-ind <deepakgera1@live.com>
2017-06-23 12:51:24 +0530

    Update update4.go

--- Create-Patient/update4.go
+++ Create-Patient/update4.go
@@ -138,20 +138,20 @@ func (t *ManagePatient) getPatient_byID(stub shim.ChaincodeStubInterface, args [
   }
   // set PatientID
    Patient_Email= args[0]
-    console.log("patient12")
+    console.log("patient12");
   PatientAsBytes, err := stub.GetState(PatientIndexStr)                  //get the PatientID from chaincode state
-    console.log("patient14")
+    console.log("patient14");
   if err != nil {
     jsonResp = "{\"Error\":\"Failed to get state for " + Patient_Email + "\"}"
     return nil, errors.New(jsonResp)
   }
-    console.log("patient15")
+    console.log("patient15");
     var PatientIndex []string
   json.Unmarshal(PatientAsBytes, &PatientIndex) 
-    console.log("patient16")
+    console.log("patient16");
   jsonResp = "{"
   for i,val := range PatientIndex{
-    console.log("patient17")
+    console.log("patient17");
     fmt.Println(strconv.Itoa(i) + " - looking at " + val + " for getPatient_byID")
     valueAsBytes, err := stub.GetState(val)
     if err != nil {
@@ -159,7 +159,7 @@ func (t *ManagePatient) getPatient_byID(stub shim.ChaincodeStubInterface, args [
       return nil, errors.New(errResp)
     }
     json.Unmarshal(valueAsBytes, &valIndex)
-       console.log("patient18")
+       console.log("patient18");
     if valIndex.Patient_Email == Patient_Email{
       fmt.Println("Patientfound")
       jsonResp = jsonResp + "\""+ val + "\":" + string(valueAsBytes[:])
@@ -174,7 +174,7 @@ func (t *ManagePatient) getPatient_byID(stub shim.ChaincodeStubInterface, args [
   //fmt.Println([]byte(jsonResp))
   fmt.Println("end getby patientID")
   return []byte(jsonResp), nil                      //send it onward
-  console.log("patient9")
+  console.log("patient9");
 }
 
 //create patient
